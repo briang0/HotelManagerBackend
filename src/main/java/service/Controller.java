1e140d5b620ea61fe27d6171000abc3f605e2f96
briang0 <briang0@iastate.edu>
2020-10-14 16:59:59 -0500

    you can now make reservations

--- src/main/java/service/Controller.java
+++ src/main/java/service/Controller.java
@@ -1,11 +1,22 @@
 package service;
 
+import db.Connector;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.Date;
+
 @RestController
 public class Controller {
+
+    Connection jdbc;
 
     //http://localhost:8080/testPhrase/?phrase="Hello"
     @RequestMapping("/testPhrase")
@@ -17,6 +28,45 @@ public class Controller {
     @RequestMapping("/test")
     public String getCode() {
         return "Hit the endpoint";
+    }
+
+    @RequestMapping("/makeReservation")
+    public String getCode(@RequestParam(value = "checkInDate") String checkIn,
+                       @RequestParam(value = "checkOutDate") String checkOut,
+                       @RequestParam(value = "reservationId") long reservationId,
+                       @RequestParam(value = "customerId") long customerId,
+                       @RequestParam(value = "rateId") long rateId,
+                       @RequestParam(value = "billId") long billId) throws ParseException {
+        System.out.println(checkIn + "\n" + checkOut + "\n" + reservationId + "\n" + customerId + "\n" + rateId + "\n" + billId);
+        Date date1 = DateUtils.parseDate(checkIn,
+                "yyyy-MM-dd HH:mm:ss", "dd/MM-yyyy");
+        Date date2 = DateUtils.parseDate(checkIn,
+                "yyyy-MM-dd HH:mm:ss", "dd/MM-yyyy");
+        java.sql.Date sqld1 = new java.sql.Date(date1.getTime());
+        java.sql.Date sqld2 = new java.sql.Date(date2.getTime());
+
+        String query = "INSERT INTO reservation VALUES(?, ?, ?, ?, ?, ?);";
+        try {
+            jdbc = Connector.getConnection("brian", "YuckyP@ssw0rd");
+            System.out.println(jdbc == null);
+            PreparedStatement p = jdbc.prepareStatement(query);
+            p.setLong(1, customerId);
+            p.setLong(2, billId);
+            p.setLong(3, rateId);
+            p.setLong(4, reservationId);
+            p.setDate(5, sqld1);
+            p.setDate(6, sqld2);
+            p.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return  "{" +
+                    "status: 401" +
+                    "}";
+        }
+        return  "{" +
+                "status: 200" +
+                "}";
+
     }
 
 }
