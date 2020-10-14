338ffd7f05b3c7da0a05fdac48722352794c1460
carp <8250820+capricorn@users.noreply.github.com>
2020-10-14 16:55:06 -0500

    Implement add and list for employee db

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -1,4 +1,41 @@
 package domain;
 
+import javax.persistence.Entity;
+
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private int employeeID;
+    private int hotelID;
+
+    public Employee(String firstName, String lastName, int employeeID, int hotelID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeID = employeeID;
+        this.hotelID = hotelID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public int getHotelID() {
+        return hotelID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- src/main/java/service/EmployeeController.java
+++ src/main/java/service/EmployeeController.java
@@ -1,4 +1,76 @@
 package service;
 
+import domain.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.Properties;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    private Connection db;
+
+    // Maybe move to a database object?
+    private void connectToDB() throws SQLException {
+        Properties props = new Properties();
+        props.put("user", "user");
+        props.put("password", "password");
+        db = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel?serverTimezone=America/Chicago",
+                props);
+    }
+
+    public EmployeeController() {
+        // maybe not the most appropriate place..
+        try {
+            connectToDB();
+        } catch (SQLException e) {
+            System.err.println("error: " + e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addEmployee(
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam int hotelID
+    ) {
+        System.out.println("Adding employee: " + firstName);
+
+        try (Statement stmt = db.createStatement()) {
+            //should actually create an employee id
+            //(presumably use primary key + autoincrement
+            //stmt.executeUpdate(String.format("INSERT INTO employee(first_name,last_name,hotel_id) values('%s', '%s', %d));",
+            stmt.executeUpdate(String.format("insert into employee(first_name, last_name, hotel_id) values('%s', '%s', %d);",
+                    firstName, lastName, hotelID));
+        } catch (SQLException e) {
+            return "failed: " + e.getMessage();
+        }
+
+        return "add successful";
+    }
+
+    // Return a list of employee objects
+    @GetMapping(path="/list")
+    public LinkedList<Employee> listEmployees() {
+        try (Statement stmt = db.createStatement()) {
+            LinkedList<Employee> employees = new LinkedList<>();
+            // pagination would maybe be better..
+            try (ResultSet result = stmt.executeQuery("select * from employee;")) {
+                // Any better way? (Maybe just define constants..
+                while (result.next()) {
+                    Employee employee = new Employee(result.getString(2), result.getString(3),
+                            result.getInt(1), result.getInt(4));
+                    employees.add(employee);
+                }
+            }
+
+            return employees;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return new LinkedList<>();
+        }
+    }
 }
