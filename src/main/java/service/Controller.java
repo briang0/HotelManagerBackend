12c9cb53e0eedd48fedd13aca0e1c53bee9e484f
briang0 <briang0@iastate.edu>
2020-10-29 03:23:13 -0500

    Finished lots of stuff for this use case and the next one

--- src/main/java/service/Controller.java
+++ src/main/java/service/Controller.java
@@ -59,7 +59,8 @@ public class Controller {
                        @RequestParam(value = "reservationId") long reservationId,
                        @RequestParam(value = "customerId") long customerId,
                        @RequestParam(value = "rateId") long rateId,
-                       @RequestParam(value = "billId") long billId) throws ParseException {
+                       @RequestParam(value = "billId") long billId,
+                       @RequestParam(value = "roomId") long roomId) throws ParseException {
         Date date1 = DateUtils.parseDate(checkIn,
                 "yyyy-MM-dd HH:mm:ss", "dd/MM-yyyy");
         Date date2 = DateUtils.parseDate(checkOut,
@@ -67,7 +68,7 @@ public class Controller {
         java.sql.Date sqld1 = new java.sql.Date(date1.getTime());
         java.sql.Date sqld2 = new java.sql.Date(date2.getTime());
 
-        String query = "INSERT INTO reservation VALUES(?, ?, ?, ?, ?, ?);";
+        String query = "INSERT INTO reservation VALUES(?, ?, ?, ?, ?, ?, ?);";
         try {
             jdbc = Connector.getConnection("brian", "YuckyP@ssw0rd");
             assert jdbc != null;
@@ -78,6 +79,7 @@ public class Controller {
             p.setDate(4, sqld1);
             p.setDate(5, sqld2);
             p.setLong(6, rateId);
+            p.setLong(7, roomId);
             p.execute();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -281,7 +283,8 @@ public class Controller {
             java.sql.Date checkIn = rs.getDate(4);
             java.sql.Date checkOut = rs.getDate(5);
             long rateId = rs.getLong(6);
-            sb.append("billId: ").append(billId).append(" Reservation Id: ").append(reservationId).append(" Check in: ").append(checkIn).append(" Check out: ").append(checkOut).append(" Rate id: ").append(rateId).append("\n");
+            long roomId = rs.getLong(7);
+            sb.append("billId: ").append(billId).append(" Reservation Id: ").append(reservationId).append(" Check in: ").append(checkIn).append(" Check out: ").append(checkOut).append(" Rate id: ").append(rateId).append(" Room id: ").append(roomId).append("\n");
         }
         result = sb.toString();
         return result;
@@ -306,6 +309,75 @@ public class Controller {
         }
         result = sb.toString();
         return result;
+    }
+
+    @RequestMapping("reservation/getAllActiveReservationsAssociatedToHotel")
+    public String getAllActiveReservationsInHotel(@RequestParam(value = "hotelId") long hotelId) throws SQLException {
+        String query = "SELECT reservation.* FROM reservation INNER JOIN room ON reservation.roomId = room.roomId WHERE room.hotelId = " + hotelId;
+        jdbc = Connector.getConnection("brian", "YuckyP@ssw0rd");
+        Statement stmt = jdbc.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        String result = "";
+        StringBuilder sb = new StringBuilder(result);
+        while(rs.next()) {
+            long customerId = rs.getLong(1);
+            long reservationId = rs.getLong(3);
+            java.sql.Date checkIn = rs.getDate(4);
+            java.sql.Date checkOut = rs.getDate(5);
+            long rateId = rs.getLong(6);
+            long roomId = rs.getLong(7);
+            java.sql.Date now = new java.sql.Date(System.currentTimeMillis());
+            if (now.before(checkOut) && now.after(checkIn))
+                sb.append("Customer ID: ").append(customerId).append(" Reservation ID: ").append(reservationId)
+                        .append(" Check in: ").append(checkIn).append(" Check Out: ").append(checkOut)
+                        .append(" rate ID: ").append(rateId).append(" Room ID: ").append(roomId).append("\n");
+        }
+        result = sb.toString();
+        return result;
+    }
+
+    @RequestMapping("reservation/getAllReservationsAssociatedToHotel")
+    public String getAllReservationsInHotel(@RequestParam(value = "hotelId") long hotelId) throws SQLException {
+        String query = "SELECT reservation.* FROM reservation INNER JOIN room ON reservation.roomId = room.roomId WHERE room.hotelId = " + hotelId;
+        jdbc = Connector.getConnection("brian", "YuckyP@ssw0rd");
+        Statement stmt = jdbc.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        String result = "";
+        StringBuilder sb = new StringBuilder(result);
+        while(rs.next()) {
+            long customerId = rs.getLong(1);
+            long reservationId = rs.getLong(3);
+            java.sql.Date checkIn = rs.getDate(4);
+            java.sql.Date checkOut = rs.getDate(5);
+            long rateId = rs.getLong(6);
+            long roomId = rs.getLong(7);
+            sb.append("Customer ID: ").append(customerId).append(" Reservation ID: ").append(reservationId)
+                    .append(" Check in: ").append(checkIn).append(" Check Out: ").append(checkOut)
+                    .append(" rate ID: ").append(rateId).append(" Room ID: ").append(roomId).append("\n");
+        }
+        result = sb.toString();
+        return result;
+    }
+
+    @RequestMapping("reservation/setWakeupTime")
+    public String setWakeupTime(@RequestParam(value = "wakeUpTime") String wakeUpTime,
+                              @RequestParam(value = "roomId") long roomId) throws ParseException {
+        String query = "UPDATE room SET wakeUpTime = ? WHERE roomId = ?;";
+        Date date1 = DateUtils.parseDate(wakeUpTime,
+                "yyyy-MM-dd HH:mm:ss", "dd/MM-yyyy");
+        java.sql.Date sqld1 = new java.sql.Date(date1.getTime());
+        try {
+            jdbc = Connector.getConnection("brian", "YuckyP@ssw0rd");
+            assert jdbc != null;
+            PreparedStatement p = jdbc.prepareStatement(query);
+            p.setDate(1, sqld1);
+            p.setLong(2, roomId);
+            p.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "{\nstatus: 400\n}";
+        }
+        return "{\nstatus: 200\n}";
     }
 
 }
