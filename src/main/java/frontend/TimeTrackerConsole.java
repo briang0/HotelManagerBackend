bc5870ab51dc455da91bfbd4ec0f6b8c22527d77
carp <8250820+capricorn@users.noreply.github.com>
2020-11-12 15:05:10 -0600

    Display current month view w/ time worked as calendar

--- src/main/java/frontend/TimeTrackerConsole.java
+++ src/main/java/frontend/TimeTrackerConsole.java
@@ -2,14 +2,17 @@ package frontend;
 
 import domain.TimeTable;
 
-import java.time.Instant;
-import java.time.LocalDate;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class TimeTrackerConsole extends SystemConsole {
     private long employeeID;
+
+    private static final HelpDisplay help = HelpDisplay.builder()
+            .add("clock", "Clock in or out.")
+            .add("time", "Display the current time worked")
+            .build();
 
     private static final ConsoleSelection clockSelection = ConsoleSelection.builder()
             .add("Check In", "clock_in")
@@ -19,7 +22,6 @@ public class TimeTrackerConsole extends SystemConsole {
     private static final ConsoleSelection timeViewSelection = ConsoleSelection.builder()
             .add("Today", "today")
             .add("Month", "month")
-            .add("Year", "year")    // multiple tables
             .build();
 
     public TimeTrackerConsole(Scanner scanner) {
@@ -53,7 +55,7 @@ public class TimeTrackerConsole extends SystemConsole {
 
     @Override
     protected void displayHelp() {
-
+        help.display();
     }
 
     public void clock() {
@@ -79,18 +81,54 @@ public class TimeTrackerConsole extends SystemConsole {
 
         if (answer.equals("today")) {
             displayTodayTime();
+        } else if (answer.equals("month")) {
+            displayMonthTime();
+        }
+    }
+
+    private void displayMonthTime() {
+        Month month = LocalDate.now().getMonth();
+        int days = LocalDate.now().lengthOfMonth();
+        int today = LocalDate.now().getDayOfMonth();
+        int weeks = LocalDate.now().lengthOfMonth() / 7 + ((days % 7 != 0) ? 1 : 0);
+
+        TableDisplay table = TableDisplay.builder(weeks, 7)
+                .enableHeader()
+                .enableColumnSeparators()
+                .build();
+
+        table.setHeader(List.of("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"));
+
+        System.out.println(days);
+
+        // This is midnight, start of day
+        long date = TimeTable.dateFromTimestamp(Instant.now().getEpochSecond());
+
+        // First of the month, as a timestamp
+        long startDate = date - (today - 2) * 86400;
+
+        String[] results = new String[days];
+        for (int i = 0; i < days; i++) {
+            // Record time worked from midnight to 11:59:59
+            Instant instant = Instant.ofEpochSecond(startDate + 86400*i);
+            String todaysDate = LocalDate.ofInstant(instant, ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("MM/dd"));
+            results[i] = String.format("%s %2.02f", todaysDate, TimeTable.timeWorked(startDate + 86400*i, startDate + 86400*i + 86399) / 60f / 60f);
         }
 
-        int weeks = LocalDate.now().lengthOfMonth() / 7;
+        // Finally, set rows
+        for (int i = 0; i < weeks; i++) {
+            int start = i*7;
+            int end = Math.min((i+1)*7, results.length);
+            //int end = (i+1)*7;
+            LinkedList<String> row = new LinkedList<>(Arrays.asList(Arrays.copyOfRange(results, start, end)));
+            // Pad extra row
+            while (row.size() < 7) row.add(" -");
+            System.out.println(row);
 
-        TableDisplay table = new TableDisplay(weeks, 7);
-        table.setHeader(List.of("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"));
-        // Then, just iterate through timestamps, start of day to end of day query via TimeTable,
-        // and display as 'MM/dd {hours:.2f}'
-        // Obtain timestamp data via time api
-        // try and mark today's date with something (could try colored output in intellij)
-        // Use '-' for empty dates
-        System.out.println(LocalDate.now().getMonth().name());
+            table.setRow(i, row);
+        }
+
+        System.out.println(month.name());
         table.display();
     }
 
@@ -98,7 +136,11 @@ public class TimeTrackerConsole extends SystemConsole {
         long time = Instant.now().getEpochSecond();
         long total = TimeTable.timeWorked(TimeTable.dateFromTimestamp(time), time);
 
-        TableDisplay table = new TableDisplay(1, 2);
+        TableDisplay table = TableDisplay.builder(1, 2)
+                .enableHeader()
+                .enableColumnSeparators()
+                .build();
+
         table.setHeader(List.of("Date", "Hours"));
         table.setRow(0, List.of(LocalDate.now().format(DateTimeFormatter.ofPattern("MM/dd E")), String.format("%.2f", total / 60f / 60f)));
         table.display();
